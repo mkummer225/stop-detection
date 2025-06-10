8147859bbbea0bf22fbf47b5adbfeda49f1491ed
Matt Kummer <7843299+mkummer225@users.noreply.github.com>
2025-06-10 17:19:45 -0600

    CTA button + initial logic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,33 @@
+import { useRef } from 'react'
 import './App.css'
 import Header from './components/header'
 import VideoPreview from './components/video-preview'
 
 function App() {
+  // Main file input reference:
+  const fileInputRef = useRef<HTMLInputElement>(null)
+
+  const handleUploadButton = () => {
+    // TODO: show filepicker and handle call to backend inference endpoint for video file
+    // fileInputRef.current...
+    return true
+  }
+
   return (
     <>
-      <Header />
-      <div className="mt-16 p-4">
+      <Header handleUploadButton={handleUploadButton} />
+
+      <div className="relative mt-16 p-4">
         <VideoPreview src={null} />
+        <input ref={fileInputRef} type="file" name="" id="" className="absolute invisible -z-10" />
 
         {/* Controls Area */}
         <div className="mt-8 flex gap-4 justify-center items-center">
           <button className='text-sm bg-zinc-100 border border-zinc-200 transition-colors hover:bg-zinc-200 cursor-pointer select-none px-2.5 py-1 rounded-lg'>Process</button>
           <button className='text-sm bg-zinc-100 border border-zinc-200 transition-colors hover:bg-zinc-200 cursor-pointer select-none px-2.5 py-1 rounded-lg'>Reset</button>
         </div>
+
+        {/* Process Results... */}
       </div>
     </>
   )
--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,11 +1,18 @@
+import { UploadIcon } from "lucide-react";
 
-export default function Header() {
+interface HeaderProps {
+    handleUploadButton: React.MouseEventHandler<HTMLButtonElement>
+}
+
+export default function Header({ handleUploadButton }: HeaderProps) {
     return (
         <div className="fixed top-0 left-0 right-0 bg-white p-4 flex justify-between items-center">
             <h1 className="text-indigo-500 font-semibold text-sm">unhappy-camper.co</h1>
 
             <div className="flex-1 text-right">
-                <button className="bg-slate-800 text-white p-4 rounded-lg px-2.5 py-1">CTA1</button>
+                <button onClick={handleUploadButton} className="inline-flex items-center text-xs uppercase bg-zinc-800 text-white hover:bg-zinc-700 transition-all hover:-translate-y-0.5 cursor-pointer p-4 rounded-lg px-5 py-2">
+                    <UploadIcon height={14} width={14} className="inline-block mr-2" />Upload Footage
+                </button>
             </div>
         </div>
     )
