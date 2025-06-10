3c3a9210f995680b8c5e274aa72b8397fcbaaa19
Matt Kummer <7843299+mkummer225@users.noreply.github.com>
2025-06-10 17:11:49 -0600

    controls

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,12 @@ function App() {
       <Header />
       <div className="mt-16 p-4">
         <VideoPreview src={null} />
+
+        {/* Controls Area */}
+        <div className="mt-8 flex gap-4 justify-center items-center">
+          <button className='text-sm bg-zinc-100 border border-zinc-200 transition-colors hover:bg-zinc-200 cursor-pointer select-none px-2.5 py-1 rounded-lg'>Process</button>
+          <button className='text-sm bg-zinc-100 border border-zinc-200 transition-colors hover:bg-zinc-200 cursor-pointer select-none px-2.5 py-1 rounded-lg'>Reset</button>
+        </div>
       </div>
     </>
   )
