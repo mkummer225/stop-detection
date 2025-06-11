6a43ac56157ba418d1663c5faf0c6afd38ef613a
Matt Kummer <7843299+mkummer225@users.noreply.github.com>
2025-06-10 18:03:13 -0600

    video preview functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react'
+import { useCallback, useMemo, useRef, useState } from 'react'
 import './App.css'
 import Header from './components/header'
 import VideoPreview from './components/video-preview'
@@ -7,24 +7,67 @@ function App() {
   // Main file input reference:
   const fileInputRef = useRef<HTMLInputElement>(null)
 
+  const [isProcessing, setIsProcessing] = useState<boolean>(false)
+  const [selectedFile, setSelectedFile] = useState<File | null>(null)
+
   const handleUploadButton = () => {
-    // TODO: show filepicker and handle call to backend inference endpoint for video file
-    // fileInputRef.current...
+    if(!fileInputRef.current) 
+      return
+
+    setSelectedFile(null)
+    fileInputRef.current?.click()
     return true
   }
+
+  const processFootage = useCallback(() => {
+    if(!fileInputRef.current?.files)
+      return
+
+    const fd = new FormData()
+    const curVideoFile = fileInputRef.current?.files[0]
+
+    fd.set("video", curVideoFile, curVideoFile.name)
+    // @ts-ignore
+    fd.set("fps", 30)
+
+    setIsProcessing(true)
+
+    fetch("http://localhost:8000/api/get_violators", {
+      method: "POST",
+      body: fd
+    }).then(async res => {
+      console.log(await res.json())
+      setIsProcessing(false)
+    }).catch(e => {
+      console.log(e)
+      setIsProcessing(false)
+    })
+  }, [selectedFile])
+
+  // Compute and memoize the current video's object URL
+  const videoObjectURL = useMemo(() => {
+    return selectedFile ? URL.createObjectURL(selectedFile) : null
+  }, [selectedFile])
 
   return (
     <>
       <Header handleUploadButton={handleUploadButton} />
 
       <div className="relative mt-16 p-4">
-        <VideoPreview src={null} />
-        <input ref={fileInputRef} type="file" name="" id="" className="absolute invisible -z-10" />
+        <VideoPreview src={videoObjectURL} isProcessing={isProcessing} />
+        <input ref={fileInputRef} type="file" accept="video/*" className="absolute invisible -z-10" onChange={() => {
+          if(!fileInputRef.current?.files) return
+          setSelectedFile(fileInputRef.current.files[0])
+        }} />
 
         {/* Controls Area */}
         <div className="mt-8 flex gap-4 justify-center items-center">
-          <button className='text-sm bg-zinc-100 border border-zinc-200 transition-colors hover:bg-zinc-200 cursor-pointer select-none px-2.5 py-1 rounded-lg'>Process</button>
-          <button className='text-sm bg-zinc-100 border border-zinc-200 transition-colors hover:bg-zinc-200 cursor-pointer select-none px-2.5 py-1 rounded-lg'>Reset</button>
+          <button className='text-sm bg-zinc-100 border border-zinc-200 transition-colors hover:bg-zinc-200 cursor-pointer select-none px-2.5 py-1 rounded-lg' onClick={processFootage}>Process</button>
+          <button className='text-sm bg-zinc-100 border border-zinc-200 transition-colors hover:bg-zinc-200 cursor-pointer select-none px-2.5 py-1 rounded-lg' onClick={() => {
+            if(!fileInputRef.current) return
+            fileInputRef.current.value = ""
+            setSelectedFile(null)
+          }}>Reset</button>
         </div>
 
         {/* Process Results... */}
--- frontend/src/components/video-preview.tsx
+++ frontend/src/components/video-preview.tsx
@@ -1,13 +1,16 @@
 
 interface VideoPreviewProps {
     src: string | null
+    isProcessing: boolean
 }
 
-export default function VideoPreview({ src }: VideoPreviewProps) {
+export default function VideoPreview({ src, isProcessing }: VideoPreviewProps) {
     return (
-        <div className="max-w-lg mx-auto bg-zinc-200 animate-pulse rounded-lg overflow-hidden">
-            {src && <video src={src} className="w-full h-auto aspect-[16/9]"></video>}
-            {!src && <div className="w-full h-auto aspect-[16/9] flex items-center justify-center text-xs font-semibold">No Video</div>}
+        <div className={"max-w-lg mx-auto bg-zinc-200 rounded-lg overflow-hidden" + (src ? '' : ' animate-pulse')}>
+            {src && <video src={src} className="w-full h-auto aspect-[16/9]" controls></video>}
+            {!src && <div className="w-full h-auto aspect-[16/9] flex items-center justify-center text-xs font-semibold">
+                {isProcessing ? 'Processing with RoboFlow...' : 'No Video'}
+            </div>}
         </div>
     )
 }
