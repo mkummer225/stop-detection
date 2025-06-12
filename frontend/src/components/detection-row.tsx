d70eccf77235fc1d3a456e0d83ed870174d89e13
Matt Kummer <7843299+mkummer225@users.noreply.github.com>
2025-06-11 21:46:29 -0600

    human readable strings

--- frontend/src/components/detection-row.tsx
+++ frontend/src/components/detection-row.tsx
@@ -5,6 +5,17 @@ interface DetectionRowProps {
 }
 
 export default function DetectionRow({ prediction }: DetectionRowProps) {
+    const FPS = 30
+
+    function secondsToHms(seconds: number) {
+        const date = new Date(seconds * 1000); // Convert seconds to milliseconds
+        const hours = date.getUTCHours();
+        const minutes = date.getUTCMinutes();
+        const secs = date.getUTCSeconds();
+      
+        return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(secs).padStart(2, '0')}`;
+    }
+
     return (
         <div className="bg-zinc-100 border border-zinc-300 p-4 rounded-lg">
             {/* {JSON.stringify(prediction)} */}
@@ -12,10 +23,10 @@ export default function DetectionRow({ prediction }: DetectionRowProps) {
                 <span className={"font-semibold" + (prediction.stopped ? " text-zinc-500" : " text-red-500")}>{ prediction.stopped ? "✅ Car Stopped" : "🛑 No STOP" }</span>
             </div>
             <div className="flex items-baseline text-sm">
-                <span className="inline-block mr-1 uppercase text-xs text-zinc-400 w-16">Begin:</span>{ prediction.start_frame }
+                <span className="inline-block mr-1 uppercase text-xs text-zinc-400 w-16">Begin:</span>{ secondsToHms(prediction.start_frame/FPS) }
             </div>
             <div className="flex items-baseline text-sm">
-                <span className="inline-block mr-1 uppercase text-xs text-zinc-400 w-16">End:</span>{ prediction.end_frame }
+                <span className="inline-block mr-1 uppercase text-xs text-zinc-400 w-16">End:</span>{ secondsToHms(prediction.end_frame/FPS) }
             </div>
 
             {/* TODO: allow user to click to jump to a detection in the video... */}
